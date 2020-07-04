db0622e8b338251d2fd352d6ced4d1b0da7d6777
sihemmessaoui <sihemmessaoui@gmail.com>
2020-07-04 20:24:07 +0200

    First commit on develop

--- app/src/main/java/com/example/orangeapplication/MainActivity.kt
+++ app/src/main/java/com/example/orangeapplication/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.orangeapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.content_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
 
-        search_view.queryHint = "Rechercher ..."
+    }
+
+    private fun initUI() {
+        recycle_view.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, 2)
+        }
     }
 }
