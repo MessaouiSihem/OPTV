533e94aff7944216f59e940e6c9756a41a23dea5
sihemmessaoui <sihemmessaoui@gmail.com>
2020-07-05 11:52:03 +0200

    Update gradle with new dependencies

--- app/src/main/java/com/example/orangeapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/orangeapplication/ui/view/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.orangeapplication
+package com.example.orangeapplication.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.orangeapplication.R
 import kotlinx.android.synthetic.main.content_layout.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun initUI() {
         recycle_view.apply {
-            layoutManager = GridLayoutManager(this@MainActivity, 2)
+           layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
     }
 }
