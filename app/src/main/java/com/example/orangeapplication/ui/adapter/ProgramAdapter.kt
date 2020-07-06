88eaa56a28571877bc9d99bfbb566a8195b1f66a
sihemmessaoui <sihemmessaoui@gmail.com>
2020-07-06 14:40:03 +0200

    update adapter to catch item click

--- app/src/main/java/com/example/orangeapplication/ui/adapter/ProgramAdapter.kt
+++ app/src/main/java/com/example/orangeapplication/ui/adapter/ProgramAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orangeapplication.R
-import com.example.orangeapplication.data.model.Contents
+import com.example.orangeapplication.data.model.program.Contents
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_program.view.*
 
